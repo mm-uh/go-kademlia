042842209608930c21355ce553cd6d6860fc7ee8
Marcos Maceo <marcosmaceowork@gmail.com>
2019-11-29 21:14:11 -0500

    Add handlers for insert in the rpc

--- src/handlers.go
+++ src/handlers.go
@@ -9,20 +9,67 @@ type HandlerRPC struct {
 }
 
 func (handler *HandlerRPC) Ping() string {
-	return "Pong"
+	return strconv.FormatBool(handler.kademlia.Ping())
 }
 
-func (handler *HandlerRPC) Store(key string, i interface{}) string {
-	return ""
+func (handler *HandlerRPC) Store(keyAsString string, i interface{}) string {
+	var key Key
+	err := key.GetFromString(keyAsString)
+	if err != nil {
+		return "false"
+	}
+	err = handler.kademlia.Store(key, i)
+	if err != nil {
+		return "false"
+	}
+	return "true"
 }
-func (handler *HandlerRPC) Get(key string) string {
-	return ""
+
+func (handler *HandlerRPC) Get(keyAsString string) string {
+	var key Key
+	err := key.GetFromString(keyAsString)
+	if err != nil {
+		return "false"
+	}
+	val, err := handler.kademlia.Get(key)
+	if err != nil {
+		return "false"
+	}
+	str, ok := val.(string)
+	if !ok {
+		return "false"
+	}
+	return str
 }
-func (handler *HandlerRPC) StoreOnNetwork(key string, i interface{}) string {
-	return ""
+
+func (handler *HandlerRPC) StoreOnNetwork(keyAsString string, i interface{}) string {
+	var key Key
+	err := key.GetFromString(keyAsString)
+	if err != nil {
+		return "false"
+	}
+	err = handler.kademlia.StoreOnNetwork(key, i)
+	if err != nil {
+		return "false"
+	}
+	return "true"
 }
-func (handler *HandlerRPC) GetFromNetwork(key string) string {
-	return ""
+
+func (handler *HandlerRPC) GetFromNetwork(keyAsString string) string {
+	var key Key
+	err := key.GetFromString(keyAsString)
+	if err != nil {
+		return "false"
+	}
+	val, err := handler.kademlia.GetFromNetwork(key)
+	if err != nil {
+		return "false"
+	}
+	str, ok := val.(string)
+	if !ok {
+		return "false"
+	}
+	return str
 }
 func (handler *HandlerRPC) GetNodeId() string {
 	return handler.kademlia.GetNodeId().GetString()
@@ -33,6 +80,20 @@ func (handler *HandlerRPC) GetIP() string {
 func (handler *HandlerRPC) GetPort() string {
 	return strconv.FormatInt(int64(handler.kademlia.GetPort()), 10)
 }
-func (handler *HandlerRPC) JoinNetwork(key, ip, port string) string {
+func (handler *HandlerRPC) JoinNetwork(keyAsString, ip, portAsString string) string {
+	var key Key
+	err := key.GetFromString(keyAsString)
+	if err != nil {
+		return "false"
+	}
+	port, err := strconv.Atoi(portAsString)
+	if err != nil {
+		return "false"
+	}
+	kdToJoin := NewKademliaContact(key, ip, port)
+	err = handler.kademlia.JoinNetwork(kdToJoin)
+	if err != nil {
+		return "false"
+	}
 	return "true"
 }
